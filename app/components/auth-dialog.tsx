b50237251cbc1106d1c15fd26074b3b2c28f88aa
Maro-Al-Kades <marooasam1@gmail.com>
2025-11-17 01:29:59 +0200

    add google auth

--- app/components/auth-dialog.tsx
+++ app/components/auth-dialog.tsx
@@ -44,6 +44,27 @@ export default function AuthDialog({ children }: AuthDialogPropsType) {
     );
   }
 
+  async function googleConnection() {
+    await authClient.signIn.social(
+      {
+        provider: "google",
+        callbackURL: window.location.href,
+      },
+      {
+        onRequest: () => {
+          setIsPending("google");
+        },
+        onSuccess: () => {
+          setIsPending(null);
+        },
+        onError: ({ error }) => {
+          setIsPending(null);
+          toast.error(error.message);
+        },
+      },
+    );
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -51,10 +72,42 @@ export default function AuthDialog({ children }: AuthDialogPropsType) {
         <DialogHeader className="space-y-3 sm:text-center">
           <DialogTitle>Faça login na plataforma</DialogTitle>
           <DialogDescription>
-            Conecte-se usando sua conta do Github
+            Conecte-se usando sua conta do Google or Github
           </DialogDescription>
         </DialogHeader>
-        <div className="mt-2">
+        <div className="mt-2 flex flex-col gap-4">
+          <Button
+            variant="outline"
+            onClick={googleConnection}
+            disabled={!!isPending}
+            className="w-full"
+          >
+            {isPending === "google" && <Loader2Icon className="animate-spin" />}
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 533.5 544.3"
+  className="mr-2 h-5 w-5"
+>
+  <path
+    fill="#4285F4"
+    d="M533.5 278.4c0-17.4-1.5-34.1-4.3-50.3H272v95.1h146.9c-6.4 34.8-25.8 64.3-54.9 84l88.9 69c51.8-47.7 81.6-118 81.6-197.8z"
+  />
+  <path
+    fill="#34A853"
+    d="M272 544.3c73.7 0 135.7-24.4 180.9-66.3l-88.9-69c-24.7 16.5-56.6 26.2-92 26.2-70.8 0-130.8-47.7-152.2-111.6l-91.2 70.5c43.7 86.4 133.3 150 243.4 150z"
+  />
+  <path
+    fill="#FBBC05"
+    d="M119.8 314.6c-10.4-31.2-10.4-64.7 0-95.9l-91.2-70.5c-39.5 78.8-39.5 171.3 0 250.1l91.2-70.5z"
+  />
+  <path
+    fill="#EA4335"
+    d="M272 107.7c39.7-.6 77.9 14.2 106.8 40.9l80.3-78.3C400.9 24.3 338.9 0 272 0 162 0 72.4 63.6 28.7 150l91.2 70.5C141.2 155.4 201.2 107.7 272 107.7z"
+  />
+</svg>
+
+            Google
+          </Button>
           <Button
             variant="outline"
             onClick={githubConnection}
